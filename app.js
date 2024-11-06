654ad072c29032702b45f706e993059a6e504c60
Oliver Smith <oliver_smith@hotmail.co.uk>
2024-11-06 14:32:04 +0000

    Finish read status logic

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// library array, with examples
 const myLibrary = [
     {
         title: 'Lord of the rings',
@@ -22,99 +23,111 @@ const myLibrary = [
         author: 'JK Rowling',
         pages: 250,
         read: true,
-    },
+    }
 ];
 
-class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-
-    }
+// Book object
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-
-
+// function passing book objects and their data through to library
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
     displayLibrary();
     console.log(myLibrary);
 }
-
 // addBookToLibrary();
-
-
 
 
 function displayLibrary() {
     const library = document.querySelector('.library');
     library.textContent = '';
+    
     for (let i  = 0; i < myLibrary.length; i++) {
+       
+        // Create book, add give it a class, append to library
         const bookItem = document.createElement('div');
         bookItem.classList.add('book-item');
         library.appendChild(bookItem);
 
+        // Create book title, append to book
         const bookTitle = document.createElement('h1');
         bookTitle.textContent = myLibrary[i].title;
         bookItem.appendChild(bookTitle);
 
+        // Create book author, append to book
         const bookAuthor = document.createElement('h2');
         bookAuthor.textContent = myLibrary[i].author;
         bookItem.appendChild(bookAuthor);
 
+        // Create book total page numbers, append to book
         const bookPages = document.createElement('p');
         bookPages.textContent = myLibrary[i].pages + ' pages';
         bookItem.appendChild(bookPages);
 
-        const bookRead = document.createElement('button');
-        bookRead.setAttribute('read-item-index', i);
-
-        if (myLibrary[i].read == true) {
-            bookRead.textContent = 'Read!';
-            bookRead.classList.add('book-read');
+        // Create 'read / unread' + check button, create the logic with checkreadstatus function, append to book.
+        const read = document.createElement("div");
+        const check=document.createElement("button");
+        check.textContent="check";
+        
+        if (myLibrary[i].read) {
+            read.textContent = 'READ';
+            read.classList.remove('book-not-read');
+            read.classList.add('book-read');
         }   else {
-            bookRead.textContent = 'Not Read';
-            bookRead.classList.add('book-not-read')
+            read.textContent = 'NOT READ';
+            read.classList.remove('book-read');
+            read.classList.add('book-not-read');
         }
-        bookItem.appendChild(bookRead);
-
-        bookRead.addEventListener('click', () => {
-            if (myLibrary[i].read = true) {
-                bookRead.textContent = 'Not Read';
-                bookRead.classList.remove('book-read');
-                bookRead.classList.add('book-not-read');
-                myLibrary[i].read = false;
-                console.log(myLibrary);
-            }   else if (myLibrary[i].read = false) {
-                bookRead.textContent = 'read';
-                bookRead.classList.remove('book-not-read');
-                bookRead.classList.add('book-read');
-                myLibrary[i].read = true;
-                console.log(myLibrary);
-            }
+        check.addEventListener('click', () => {
+            checkReadStatus(myLibrary[i].title)
         });
 
+        bookItem.appendChild(read);
+        bookItem.appendChild(check);
+
+        // Create delete button, create logic, append to book
         const deletebtn = document.createElement('button');
         deletebtn.textContent = 'Delete';
-        deletebtn.classList.add('deleteBtn');
+        
         deletebtn.setAttribute('delete-item-index', i);
-        bookItem.appendChild(deletebtn);
-
-
         deletebtn.addEventListener('click', () => {
             myLibrary.splice(deletebtn.getAttribute('delete-item-index'), 1);
             displayLibrary();
             console.log(myLibrary);
-        })
+        });
+
+        bookItem.appendChild(deletebtn);
+
     }
 
 }
 displayLibrary();
+console.log(myLibrary);
 
 
+function checkReadStatus(title){
+    myLibrary.forEach(element=>{
+        if(element.title == title){
+            if(element.read == true){
+                element.read = false;
+            }
+            else{
+                element.read = true;
+            }
+        }
+    });
+    displayLibrary();
+    console.log(myLibrary);
+}
+
+
+// send form function
 function addBook(e) {
     e.preventDefault();
     const inputTitle = document.querySelector('#input-title');
@@ -123,6 +136,7 @@ function addBook(e) {
     const inputRead = document.querySelector('#input-read');
     const form = document.querySelector('form');
 
+    // if title and author are not empty and pages is not 0, check to see if read is checked or not, then send values of each input to addBookToLibrary
     if (inputTitle.value !== '' && inputAuthor.value !== '' && inputPages.value > 0) {
         if (inputRead.checked) {
             addBookToLibrary(inputTitle.value, inputAuthor.value, inputPages.value, true);
@@ -132,27 +146,7 @@ function addBook(e) {
             form.reset();
         }
     };
-
-
 }
 
 const addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', addBook);
-
-
-
-// function deleteBook() {
-//     document.addEventListener('click', e => {
-//         const { target } = e;
-
-//         if (target.classList.contains('deleteBtn')) {
-//             console.log(target.parentNode);
-//             // myLibrary.splice(target.parentNode.parentNode, 1);
-//             let removeMe = target.parentElement;
-//             myLibrary.filter(removeMe);
-//             console.log(myLibrary);
-//         }
-//     })
-// }
-
-// deleteBook();
